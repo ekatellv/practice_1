36d07af0793ad41102d4e02c2736336517a6a810
ekatellv <159680448+ekatellv@users.noreply.github.com>
2024-06-19 22:46:44 +0700

    Create ex_8.py

---
+++ ex_8.py
@@ -0,0 +1,4 @@
+login = input('Введите логин:')
+password = input('Введите пароль:')
+new_password = input('Введите новый пароль:')
+print('User', login, 'has changed the password to', new_password )
